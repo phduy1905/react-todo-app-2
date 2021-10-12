1d8b312b853b7a25a67cbc39f3a94c114946181c
phduy1905 <phduy1905@gmail.com>
2021-10-12 19:11:46 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { LeftSide, RightSide } from "./components/index";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <LeftSide />
+      <RightSide />
+    </div>
+  );
+}
+
+export default App;
